2d3c326b9822c5eb30b88672518bd3e294c4ae62
Steven Contreras <SFContra@gmail.com>
2019-12-05 15:19:24 -0800

    nearly complete

--- client/src/components/PokeTeam.jsx
+++ client/src/components/PokeTeam.jsx
@@ -9,3 +9,18 @@
   PokemonTeamSlot
 </div>
 */
+import React from 'react';
+import PokeTeamSlot from './PokeTeamSlot.jsx';
+
+const PokeTeam = (props) => {
+  (props.pokemon) 
+  ? (
+      <div id="pokeTeam">
+        {props.pokeTeam.map((pokemon, index) => (<PokeTeamSlot pokemon={pokemon} key={index} addPokemon={props.addPokemon}/>))}
+      </div>
+  ) : (
+    <button>Add a Pokemon</button>
+  );
+};
+
+export default PokeTeam
